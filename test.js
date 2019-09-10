e4fc04021cbad5f36b281b40cc114274700b6003
Prasad Domala <prasad.domala@gmail.com>
2019-09-10 22:04:14 +1000

    deleted test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const AWS = require('aws-sdk');
-const keys = {
-    accessKeyId: 'AKIARLWHPC4T54QZ5I74',
-    secretAccessKey: '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'
-};
-
-const sts = new AWS.STS(keys);
-
-console.log(sts);
